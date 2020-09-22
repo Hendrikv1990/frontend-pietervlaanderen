cfa21963b8df655ab38a61328eb775bc48c84170
Hendrik Vlaanderen <h.k.vlaanderen@gmail.com>
2020-09-22 20:50:47 +0200

    Cookiebot & Snapping, Data protection page

--- propTypes/data-protection-policy.js
+++ propTypes/data-protection-policy.js
@@ -5,10 +5,8 @@ export const dataProtectionPolicyPropType = () => PropTypes.shape({
 	title: PropTypes.array,
 	subtitle: PropTypes.array,
 	full_description: PropTypes.array,
-
 	seo_title: PropTypes.string,
 	seo_meta_description: PropTypes.string,
-
 	_meta: PropTypes.shape({
 		id: PropTypes.string,
 		uid: PropTypes.string
